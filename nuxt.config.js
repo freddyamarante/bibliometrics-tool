6fab43781ec1240e53b402654f3c236bf201a2af
Freddy Amarante <freddyamarante14@gmail.com>
2022-08-09 18:30:41 -0500

    Setup database service

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,26 +9,22 @@ export default {
   head: {
     title: 'bibliometrics-tool',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -45,12 +41,30 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: 'AIzaSyAbNMQG40XHsWSRZrWFmoFiccSDwtPI1PM',
+          authDomain: 'bibliometrics-tool.firebaseapp.com',
+          databaseURL: 'https://bibliometrics-tool-default-rtdb.firebaseio.com',
+          projectId: 'bibliometrics-tool',
+          storageBucket: 'bibliometrics-tool.appspot.com',
+          messagingSenderId: '581215064675',
+          appId: '1:581215064675:web:27818fc74086da9161de56',
+          measurementId: 'G-PP0S44DHCW',
+        },
+        services: {
+          auth: true,
+          database: true,
+        },
+      },
+    ],
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
